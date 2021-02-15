7c608fabe1e9487af0332f61fecaaf7275edb550
Anh Nguyen <nguyena.r216@gmail.com>
2021-02-15 16:13:48 -0500

    Initial Commit

---
+++ FlixApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlixApp
+//
+//  Created by Anh Nguyen on 2/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
