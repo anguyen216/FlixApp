da4fb3b477f5c76449806eac75318d101fa46f85
Anh Nguyen <nguyena.r216@gmail.com>
2021-02-24 18:44:48 -0500

    add details to details view; add auto-layout for detail view

--- FlixApp/MovieDetailsViewController.swift
+++ FlixApp/MovieDetailsViewController.swift
@@ -6,15 +6,40 @@
 //
 
 import UIKit
+import Alamofire
 
 class MovieDetailsViewController: UIViewController {
 
     var movie: [String: Any]!
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        print(movie["title"])
+        // add movie title and synopsis
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        // get movie poster
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        posterView.af.setImage(withURL: posterUrl!)
+        
+        // set white border to poster view
+        posterView.layer.masksToBounds = true
+        posterView.layer.borderWidth = 1
+        posterView.layer.borderColor = UIColor.white.cgColor
+        
+        // get movie backdrop poster
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        backdropView.af.setImage(withURL: backdropUrl!)
     }
     
 
